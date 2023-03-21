49c602cfea8b22b635023e10b08add433cb75568
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2023-03-21 12:08:52 +0900

    ADD: Calendar style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import { Calendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function MyCalendar() {
+  const customStyles = {
+    width: "1800px",
+    height: "1600px",
+  };
+
   return (
     <div>
-      <Calendar />
+      <Calendar style={customStyles} />
     </div>
   );
 }
